b05a3acaf4016c72b7fce91a4dd37e6508a7315f
Gustavo <trabajoglm2021@gmail.com>
2025-01-13 22:57:58 -0600

    Obteniendo Persona segun Deal

--- controllers/followBossController.js
+++ controllers/followBossController.js
@@ -26,10 +26,15 @@ export const getGoogleSheet = async (req, res) => {
 export const getWebhooks = async (req, res) => {
     const webhookData = req.headers;
     console.log('----- Webhook recibido -----');
-   
-    const body = req.body
-    const dealUri = body.dealUri
+
+    //obteniendo la direccion del Deal Editado
+    const dealUri = req.body.uri
+    const personId = await obtenerPersonId(dealUri);
+    const person = await cargarPerson(personId);
+   // const dealUri = body.uri
     console.log("Deal Editado:", dealUri)
+    console.log("Person Id:" , personId)
+    console.log("Person:" , person)
     try {        
         res.status(200).send('Webhook recibido');
     } catch (error) {
@@ -37,3 +42,28 @@ export const getWebhooks = async (req, res) => {
         res.status(500).send('Error al obtener google sheet');
     }
 };
+async function obtenerPersonId(dealUri){
+    const apiKey = process.env.FOLLOW_BOSS_API_KEY;
+    const options = {
+        method: 'GET' , headers: {
+            'Authorization': 'Basic ' + btoa(API_KEY + ':')
+        }
+    }
+    const response = await fetch(dealUri, options);
+    const data = await response.json();
+    return data.people.id
+}
+async function cargarPerson(personId) {
+    const apiKey = process.env.FOLLOW_BOSS_API_KEY;
+    let url = `https://api.followupboss.com/v1/people/${personId}`;
+    const options = {
+        method: 'GET', headers: {
+            'Authorization': 'Basic ' + btoa(API_KEY + ':') // Usamos 'Basic' y la clave API codificada en base64
+        }
+    }
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data
+}
+
+
